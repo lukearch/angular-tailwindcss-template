54a9c2f16420e58bb7b2124d55c39798b2837434
Lucas Larangeira <lukearch.sh@gmail.com>
2024-03-21 00:29:27 -0300

    Add Prettier configuration, VS Code settings, and format script*** ***Update package.json with Prettier format script*** ***Update server.ts with import statement*** ***Update app.config.ts with Angular router imports*** ***Update styles.scss with Tailwind CSS imports*** ***Update tailwind.config.js with content path*** ***Update yarn.lock with new dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,25 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withHttpTransferCacheOptions,
+} from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideClientHydration(
+      withHttpTransferCacheOptions({
+        includePostRequests: true,
+      })
+    ),
+    provideHttpClient(withFetch()),
+  ],
 };
